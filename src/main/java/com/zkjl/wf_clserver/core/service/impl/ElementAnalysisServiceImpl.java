dc4521503507af2ce0609d91539efc4a2e7b6a6c
17780 <1778078767@qq.com>
2018-07-07 11:26:16 +0800

    增加

--- src/main/java/com/zkjl/wf_clserver/core/service/impl/ElementAnalysisServiceImpl.java
+++ src/main/java/com/zkjl/wf_clserver/core/service/impl/ElementAnalysisServiceImpl.java
@@ -1,17 +1,16 @@
 package com.zkjl.wf_clserver.core.service.impl;
 
-import com.alibaba.fastjson.JSONObject;
 import com.zkjl.wf_clserver.core.common.ApiResult;
 import com.zkjl.wf_clserver.core.entity.CollDatas;
 import com.zkjl.wf_clserver.core.entity.CollDatasInner;
 import com.zkjl.wf_clserver.core.repository.plover.CollDatasRepository;
-import com.zkjl.wf_clserver.core.repository.plover.JobBeanRepository;
 import com.zkjl.wf_clserver.core.service.AnalysisAbstractService;
 import com.zkjl.wf_clserver.core.service.ElementAnalysisService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 /**
@@ -36,7 +35,19 @@ public class ElementAnalysisServiceImpl extends AnalysisAbstractService implemen
 
     @Override
     protected void analysisSameAddress(List<List<CollDatas>> datas) {
-
+        if (datas.size() == 0) {
+            throw new RuntimeException();
+        }
+        List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同住址");
+        System.out.print("yunsoux同住址"+kindDatas);
+        String address1 = kindDatas.get(0).get(0).getData().getData()[0][7];
+        String address2 = kindDatas.get(1).get(0).getData().getData()[0][7];
+        System.out.println("address1:"+address1);
+        System.out.println("address2:"+address2);
+        if(address1.equals(address2)){
+            System.out.println("含有相同住址");
+        }
+        System.out.println("!!!!!!!!!!");
     }
 
     @Override
@@ -46,7 +57,7 @@ public class ElementAnalysisServiceImpl extends AnalysisAbstractService implemen
 
     @Override
     protected void analysisSameWork(List<List<CollDatas>> datas) {
-        String idcard = datas.get(0).get(0).getData().get(0).getData().getData()[0][7];
+        String idcard = datas.get(0).get(0).getData().get(0).getData().getData()[0][0];
         List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同机构");
         List<CollDatasInner> collDatasInners2 = kindDatas.get(1);
         String[][] data = collDatasInners2.get(0).getData().getData();
@@ -63,6 +74,21 @@ public class ElementAnalysisServiceImpl extends AnalysisAbstractService implemen
 
     @Override
     protected void analysisSameViolation(List<List<CollDatas>> datas) {
+        String idcard = datas.get(0).get(0).getData().get(0).getData().getData()[0][0];
+        System.out.println("同车违章"+idcard);
+        List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同车违章");
+        System.out.println("同车违章"+kindDatas);
+        List<CollDatasInner> collDatasInners2 = kindDatas.get(1);
+        String[][] data = collDatasInners2.get(0).getData().getData();
+        List<String> ids = new ArrayList<>();
+        for (int i = 0; i < data.length; i++) {
+            String[] datum = data[i];
+            String id = datum[7];
+            ids.add(id);
+        }
+        if(ids.contains(idcard)){
+            System.out.println("1同车违章");
+        }
 
     }
 
@@ -87,6 +113,7 @@ public class ElementAnalysisServiceImpl extends AnalysisAbstractService implemen
             throw new RuntimeException();
         }
         List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同户号");
+        System.out.print("yunsoux户号"+kindDatas);
         String account1 = kindDatas.get(0).get(0).getData().getData()[0][7];
         String account2 = kindDatas.get(1).get(0).getData().getData()[0][7];
         System.out.println("account1:"+account1);
--- src/main/resources/static/js/x-admin.js
+++ src/main/resources/static/js/x-admin.js
@@ -1,7 +1,7 @@
 layui.use(['element'], function(){
 	$ = layui.jquery;
-  	element = layui.element(); 
-  
+  	element = layui.element();
+
   //导航的hover效果、二级菜单等功能，需要依赖element模块
   // larry-side-menu向左折叠
   	if($(window).width()<750){
@@ -46,6 +46,69 @@ $('.larry-side-menu').click(function() {
 
     	$('.layui-tab-title li').eq(0).find('i').remove();
   });
+
+
 });
+
+var $,tab,skyconsWeather;
+layui.config({
+	base : "js/"
+}).use(['form','element','layer','jquery'],function(){
+	var form = layui.form(),
+		layer = layui.layer,
+		element = layui.element();
+		$ = layui.jquery;
+var name = localStorage.getItem("name");//获取b的值
+   var photo = localStorage.getItem("photo");//获取a的值
+	//锁屏
+	function lockPage(){
+		layer.open({
+			title : false,
+			type : 1,
+			content : '	<div class="admin-header-lock" id="lock-box" style="width: 320px;height: 170px;padding: 20px;position: relative;text-align: center;">'+
+							'<div class="admin-header-lock-img" style=" width: 60px;height: 60px;margin: 0 auto;"><img style="width: 60px;height: 60px;border-radius: 100%;" src='+photo+'></div>'+
+							'<div class="admin-header-lock-name" style="color: #009688;margin: 8px 0 15px 0;" id="lockUserName">'+name+'</div>'+
+							'<div class="input_btn" style=" overflow: hidden;margin-bottom: 10px;">'+
+								'<input type="password" class="admin-header-lock-input layui-input" autocomplete="off" placeholder="请输入密码解锁.." name="lockPwd" id="lockPwd" style="width: 170px;color: #fff;background-color: #009688;float: left;margin: 0 10px 0 40px;border: none;"/>'+
+								'<button class="layui-btn" id="unlock" style="float: left;background-color: #009688;color: #fff;">解锁</button>'+
+							'</div>'+
+							'<p style="color: #e60000;">请输入您的密码，否则不会解锁成功哦！！！</p>'+
+						'</div>',
+			closeBtn : 0,
+			shade : 0.9
+		})
+		$(".admin-header-lock-input").focus();
+	}
+	$(".lockcms").on("click",function(){
+		window.sessionStorage.setItem("lockcms",true);
+		lockPage();
+	})
+	// 判断是否显示锁屏
+	if(window.sessionStorage.getItem("lockcms") == "true"){
+		lockPage();
+	}
+	// 解锁
+	$("body").on("click","#unlock",function(){
+		if($(this).siblings(".admin-header-lock-input").val() == ''){
+			layer.msg("请输入解锁密码！");
+			$(this).siblings(".admin-header-lock-input").focus();
+		}else{
+			if($(this).siblings(".admin-header-lock-input").val() == "123456"){
+				window.sessionStorage.setItem("lockcms",false);
+				$(this).siblings(".admin-header-lock-input").val('');
+				layer.closeAll("page");
+			}else{
+				layer.msg("密码错误，请重新输入！");
+				$(this).siblings(".admin-header-lock-input").val('').focus();
+			}
+		}
+	});
+	$(document).on('keydown', function() {
+		if(event.keyCode == 13) {
+			$("#unlock").click();
+		}
+	});
+
+})
 
 
