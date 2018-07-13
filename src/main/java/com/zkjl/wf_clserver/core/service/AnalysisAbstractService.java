9c75e57c8997f425c952d08e778e9cfd73b91782
260446015@qq.com <260446015@qq.com>
2018-07-13 16:55:24 +0800

    提交代码

--- src/main/java/com/zkjl/wf_clserver/core/service/AnalysisAbstractService.java
+++ src/main/java/com/zkjl/wf_clserver/core/service/AnalysisAbstractService.java
@@ -1,14 +1,10 @@
 package com.zkjl.wf_clserver.core.service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.zkjl.wf_clserver.core.common.ApiResult;
-import com.zkjl.wf_clserver.core.entity.CollDatas;
-import com.zkjl.wf_clserver.core.entity.CollDatasInner;
-import org.apache.commons.lang.StringUtils;
+import org.bson.Document;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author ydw
@@ -17,7 +13,7 @@ import java.util.stream.Collectors;
 public abstract class AnalysisAbstractService {
     protected JSONObject analysis(String word1, String word2) {
         JSONObject result = new JSONObject();
-        List<List<CollDatas>> cacheDatasByJobId = getCacheDatasByJobId(word1, word2);
+        List<List<Document>> cacheDatasByJobId = getCacheDatasByJobId(word1, word2);
         result.putAll(analysisSameAccount(cacheDatasByJobId));
         result.putAll(analysisSameCase(cacheDatasByJobId));
         result.putAll(analysisSameRoom(cacheDatasByJobId));
@@ -30,65 +26,65 @@ public abstract class AnalysisAbstractService {
         return result;
     }
 
-    protected List<List<CollDatasInner>> getKindDatas(List<List<CollDatas>> datas, String platKind, String label) {
-        List<List<CollDatasInner>> resultDatas = new ArrayList<>(2);
-        List<CollDatas> datas1 = datas.get(0).stream().filter(action -> action.getResid().equals(platKind)).collect(Collectors.toList());
-        List<CollDatas> datas2 = datas.get(1).stream().filter(action -> action.getResid().equals(platKind)).collect(Collectors.toList());
-        List<CollDatasInner> collDatasInners1 = datas1.get(0).getData();
-        List<CollDatasInner> collDatasInners2 = datas2.get(0).getData();
-        if(!StringUtils.isBlank(label)){
-            collDatasInners1 = datas1.get(0).getData().stream().filter(action -> action.getLabel().equals(label)).collect(Collectors.toList());
-            collDatasInners2 = datas2.get(0).getData().stream().filter(action -> action.getLabel().equals(label)).collect(Collectors.toList());
-        }
-        resultDatas.add(collDatasInners1);
-        resultDatas.add(collDatasInners2);
+    protected List<List<Document>> getKindDatas(List<List<Document>> datas, String platKind, String label) {
+        List<List<Document>> resultDatas = new ArrayList<>(2);
+//        List<Document> datas1 = datas.get(0).stream().filter(action -> action.getResid().equals(platKind)).collect(Collectors.toList());
+//        List<Document> datas2 = datas.get(1).stream().filter(action -> action.getResid().equals(platKind)).collect(Collectors.toList());
+//        List<Document> DocumentInners1 = datas1.get(0).getData();
+//        List<Document> DocumentInners2 = datas2.get(0).getData();
+//        if(!StringUtils.isBlank(label)){
+//            DocumentInners1 = datas1.get(0).getData().stream().filter(action -> action.getLabel().equals(label)).collect(Collectors.toList());
+//            DocumentInners2 = datas2.get(0).getData().stream().filter(action -> action.getLabel().equals(label)).collect(Collectors.toList());
+//        }
+//        resultDatas.add(DocumentInners1);
+//        resultDatas.add(DocumentInners2);
         return resultDatas;
     }
 
     /**
      * 同会员
      */
-    protected abstract JSONObject analysisSameMember(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameMember(List<List<Document>> datas);
 
     /**
      * 同住址
      */
-    protected abstract JSONObject analysisSameAddress(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameAddress(List<List<Document>> datas);
 
     /**
      * 同手机号
      */
-    protected abstract JSONObject analysisSamePhone(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSamePhone(List<List<Document>> datas);
 
     /**
      * 同单位
      */
-    protected abstract JSONObject analysisSameWork(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameWork(List<List<Document>> datas);
 
     /**
      * 同车违章
      */
-    protected abstract JSONObject analysisSameViolation(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameViolation(List<List<Document>> datas);
 
     /**
      * 同上网
      */
-    protected abstract JSONObject analysisSameInet(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameInet(List<List<Document>> datas);
 
     /**
      * 同住
      */
-    protected abstract JSONObject analysisSameRoom(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameRoom(List<List<Document>> datas);
 
     /**
      * 同案
      */
-    protected abstract JSONObject analysisSameCase(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameCase(List<List<Document>> datas);
 
     /**
      * 同户
      */
-    protected abstract JSONObject analysisSameAccount(List<List<CollDatas>> datas);
+    protected abstract JSONObject analysisSameAccount(List<List<Document>> datas);
 
     /**
      * 获取数据库中缓存数据
@@ -97,7 +93,7 @@ public abstract class AnalysisAbstractService {
      * @param jobid2
      * @return
      */
-    protected abstract List<List<CollDatas>> getCacheDatasByJobId(String jobid1, String jobid2);
+    protected abstract List<List<Document>> getCacheDatasByJobId(String jobid1, String jobid2);
 
     /**
      * 获取目标数据
--- src/main/java/com/zkjl/wf_clserver/core/service/impl/ElementAnalysisServiceImpl.java
+++ src/main/java/com/zkjl/wf_clserver/core/service/impl/ElementAnalysisServiceImpl.java
@@ -1,20 +1,19 @@
 package com.zkjl.wf_clserver.core.service.impl;
 
 import com.alibaba.fastjson.JSONObject;
-import com.zkjl.wf_clserver.core.common.ApiResult;
-import com.zkjl.wf_clserver.core.entity.CollDatas;
-import com.zkjl.wf_clserver.core.entity.CollDatasInner;
-import com.zkjl.wf_clserver.core.repository.plover.CollDatasRepository;
 import com.zkjl.wf_clserver.core.service.AnalysisAbstractService;
 import com.zkjl.wf_clserver.core.service.ElementAnalysisService;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author ydw
@@ -23,8 +22,8 @@ import java.util.stream.Collectors;
 @Service
 public class ElementAnalysisServiceImpl extends AnalysisAbstractService implements ElementAnalysisService {
 
-    @Resource
-    private CollDatasRepository collDatasRepository;
+    @Resource(name = "secondaryMongoTemplate")
+    private MongoTemplate secondMongoTemplate;
 
     private static Logger logger = LoggerFactory.getLogger(ElementAnalysisServiceImpl.class);
 
@@ -34,140 +33,58 @@ public class ElementAnalysisServiceImpl extends AnalysisAbstractService implemen
     }
 
     @Override
-    protected JSONObject analysisSameMember(List<List<CollDatas>> datas) {
+    protected JSONObject analysisSameMember(List<List<Document>> datas) {
         return null;
     }
 
     @Override
-    protected JSONObject analysisSameAddress(List<List<CollDatas>> datas) {
-        if (datas.size() == 0) {
-            throw new RuntimeException();
-        }
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("sameAddress",null);
-        try {
-        List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同住址");
-        String address1 = kindDatas.get(0).get(0).getData().getData()[0][7];
-        String address2 = kindDatas.get(1).get(0).getData().getData()[0][7];
-        System.out.println("address1:"+address1);
-        System.out.println("address2:"+address2);
-        if(address1.equals(address2)){
-            jsonObject.put("sameAddress",address1);
-        }
-        } catch (Exception e) {
-            logger.error("查询同住址出现异常:",e.getMessage());
-        }
-        return jsonObject;
-    }
-
-    @Override
-    protected JSONObject analysisSamePhone(List<List<CollDatas>> datas) {
+    protected JSONObject analysisSameAddress(List<List<Document>> datas) {
         return null;
     }
 
     @Override
-    protected JSONObject analysisSameWork(List<List<CollDatas>> datas) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("sameWork",null);
-        try {
-        String idcard = datas.get(0).get(0).getData().get(0).getData().getData()[0][0];
-        List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同机构");
-        List<CollDatasInner> collDatasInners2 = kindDatas.get(1);
-        String[][] data = collDatasInners2.get(0).getData().getData();
-        List<String> ids = new ArrayList<>();
-        for (int i = 0; i < data.length; i++) {
-            String[] datum = data[i];
-            String id = datum[0];
-            ids.add(id);
-            if(id.equals(idcard)){
-                jsonObject.put("sameWork",datum);
-            }
-        }
-        /*if(ids.contains(idcard)){
-            jsonObject.put("sameWork",kindDatas.get(0));
-        }*/
-        } catch (Exception e) {
-            logger.error("查询同机构出现异常:",e.getMessage());
-        }
-        return jsonObject;
-    }
-
-    @Override
-    protected JSONObject analysisSameViolation(List<List<CollDatas>> datas) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("sameWork",null);
-        try {
-        String idcard = datas.get(0).get(0).getData().get(0).getData().getData()[0][0];
-        System.out.println("同车违章"+idcard);
-        List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同车违章");
-        System.out.println("同车违章"+kindDatas);
-        List<CollDatasInner> collDatasInners2 = kindDatas.get(1);
-        String[][] data = collDatasInners2.get(0).getData().getData();
-        List<String> ids = new ArrayList<>();
-        for (int i = 0; i < data.length; i++) {
-            String[] datum = data[i];
-            String id = datum[7];
-            ids.add(id);
-            if(id.equals(idcard)){
-                jsonObject.put("sameViolation",datum);
-            }
-        }
-        if(ids.contains(idcard)){
-            System.out.println("同车违章");
-        }
-        } catch (Exception e) {
-            logger.error("查询同车违章出现异常:",e.getMessage());
-        }
-        return jsonObject;
-    }
-
-    @Override
-    protected JSONObject analysisSameInet(List<List<CollDatas>> datas) {
+    protected JSONObject analysisSamePhone(List<List<Document>> datas) {
         return null;
     }
 
     @Override
-    protected JSONObject analysisSameRoom(List<List<CollDatas>> datas) {
+    protected JSONObject analysisSameWork(List<List<Document>> datas) {
         return null;
     }
 
     @Override
-    protected JSONObject analysisSameCase(List<List<CollDatas>> datas) {
+    protected JSONObject analysisSameViolation(List<List<Document>> datas) {
         return null;
     }
 
     @Override
-    protected JSONObject analysisSameAccount(List<List<CollDatas>> datas) {
-        if (datas.size() == 0) {
-            throw new RuntimeException();
-        }
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("sameAccount",null);
-        try {
-            List<List<CollDatasInner>> kindDatas = getKindDatas(datas, "yunsou", "同户号");
-            System.out.print("yunsoux户号"+kindDatas);
-            String account1 = kindDatas.get(0).get(0).getData().getData()[0][7];
-            String account2 = kindDatas.get(1).get(0).getData().getData()[0][7];
-            System.out.println("account1:"+account1);
-            System.out.println("account2:"+account2);
-            if(account1.equals(account2)){
-                jsonObject.put("sameAccount",kindDatas.get(0));
-            }
-        } catch (Exception e) {
-            logger.error("查询同户号出现异常:",e.getMessage());
-        }
-        return jsonObject;
+    protected JSONObject analysisSameInet(List<List<Document>> datas) {
+        return null;
     }
 
     @Override
-    protected List<List<CollDatas>> getCacheDatasByJobId(String jobId1, String jobId2) {
-        List<List<CollDatas>> list = new ArrayList<>(2);
-        List<CollDatas> collDatas1 = collDatasRepository.findByJobid(jobId1);
-        List<CollDatas> collDatas2 = collDatasRepository.findByJobid(jobId2);
-        List<CollDatas> filterList1 = collDatas1.stream().filter(action -> action.getData().size() != 0).collect(Collectors.toList());
-        List<CollDatas> filterList2 = collDatas2.stream().filter(action -> action.getData().size() != 0).collect(Collectors.toList());
-        list.add(filterList1);
-        list.add(filterList2);
+    protected JSONObject analysisSameRoom(List<List<Document>> datas) {
+        return null;
+    }
+
+    @Override
+    protected JSONObject analysisSameCase(List<List<Document>> datas) {
+        return null;
+    }
+
+    @Override
+    protected JSONObject analysisSameAccount(List<List<Document>> datas) {
+        return null;
+    }
+
+
+    @Override
+    protected List<List<Document>> getCacheDatasByJobId(String jobId1, String jobId2) {
+        List<List<Document>> list = new ArrayList<>(2);
+        List<Document> documents = secondMongoTemplate.find(new Query(Criteria.where("jobid").is(jobId1)), Document.class, "coll_datas");
+        List<Document> documents2 = secondMongoTemplate.find(new Query(Criteria.where("jobid").is(jobId2)), Document.class, "coll_datas");
+        list.add(documents);
+        list.add(documents2);
         return list;
     }
 
