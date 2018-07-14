7b2afd616b19bdbe8e07f7fce6c85e1e97c6dcc7
260446015@qq.com <260446015@qq.com>
2018-07-14 16:37:03 +0800

    解决冲突

--- src/main/java/com/zkjl/wf_clserver/core/controller/LinkController.java
+++ src/main/java/com/zkjl/wf_clserver/core/controller/LinkController.java
@@ -7,16 +7,16 @@ import com.zkjl.wf_clserver.core.entity.Link;
 import com.zkjl.wf_clserver.core.service.LinkService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * 超级链接
  */
 @RestController
-@RequestMapping("/link")
+@RequestMapping("/api/link")
 @Api(value = "Link-API", description = "这是超级链接详细信息的描述")
 public class LinkController extends BaseController {
     @Resource
@@ -28,15 +28,16 @@ public class LinkController extends BaseController {
     @GetMapping("/findAll")
     @SystemControllerLog(description = "查询超链接")
     @ApiOperation(value = "查询超链接", httpMethod = "GET")
-    public ApiResult findAll() throws Exception {
-        List<Link> links = linkService.findAll();
-        return success(links);
+    public ApiResult findAll(String search, Integer pageNum, Integer pageSzie) throws Exception {
+        PageImpl<Link> links = linkService.findAll(search, pageNum, pageSzie);
+        return successPages(links);
     }
 
     /**
      * 链接添加或修改
      */
     @PostMapping("/save")
+    @SystemControllerLog(description = "超链接添加/修改")
     @ApiOperation(value = "链接添加", httpMethod = "POST")
     public ApiResult save(@RequestBody Link link) throws Exception {
         return success(linkService.saveOrUpdate(link));
