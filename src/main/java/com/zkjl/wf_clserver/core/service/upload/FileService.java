2cd1f6a3e5c65440bceca2d2e7aa17614ac953ca
260446015@qq.com <260446015@qq.com>
2018-07-14 17:17:35 +0800

    解决冲突

--- src/main/java/com/zkjl/wf_clserver/core/service/upload/FileService.java
+++ src/main/java/com/zkjl/wf_clserver/core/service/upload/FileService.java
@@ -1,10 +1,12 @@
 package com.zkjl.wf_clserver.core.service.upload;
 
+import com.alibaba.fastjson.JSONObject;
 import com.zkjl.wf_clserver.core.entity.FileUploadEntity;
 import com.zkjl.wf_clserver.core.exception.CustomerException;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 /**
@@ -13,7 +15,9 @@ import java.io.IOException;
  */
 public interface FileService {
 
-    void upload(MultipartFile file) throws IOException, CustomerException;
+    JSONObject upload(MultipartFile file, HttpServletRequest request) throws IOException, CustomerException;
 
     PageImpl<FileUploadEntity> search(String search, int pageNum, int pageSize);
+
+    boolean delete(String source);
 }
