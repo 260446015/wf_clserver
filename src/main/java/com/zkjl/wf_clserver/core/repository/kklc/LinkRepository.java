d9374b6b9bb9d427a10cef9c0e239d6342d3aec9
260446015@qq.com <260446015@qq.com>
2018-07-14 17:05:34 +0800

    解决冲突

--- src/main/java/com/zkjl/wf_clserver/core/repository/kklc/LinkRepository.java
+++ src/main/java/com/zkjl/wf_clserver/core/repository/kklc/LinkRepository.java
@@ -3,6 +3,9 @@ package com.zkjl.wf_clserver.core.repository.kklc;
 import com.zkjl.wf_clserver.core.entity.Link;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface LinkRepository extends MongoRepository<Link,String> {
 
+    List<Link> findByName(String search);
 }
