0dac6336e7debfd02afb43e56a2b907ccc5d7291
260446015@qq.com <260446015@qq.com>
2018-07-14 16:42:44 +0800

    解决冲突

--- src/main/java/com/zkjl/wf_clserver/core/dto/req/DefaultPageRQ.java
+++ src/main/java/com/zkjl/wf_clserver/core/dto/req/DefaultPageRQ.java
@@ -1,8 +1,15 @@
 package com.zkjl.wf_clserver.core.dto.req;
 
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+
+import java.util.Date;
 
 @ApiModel
+@Data
 public class DefaultPageRQ extends AbstractPageRQ{
     private static final long serialVersionUID = -4045422720156943459L;
+    private String name;
+    private Date beginDate;
+    private Date endDate;
 }
