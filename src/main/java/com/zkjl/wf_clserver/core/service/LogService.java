2ed588e8d1ff55829883dab4ebe5f45f336e6668
260446015@qq.com <260446015@qq.com>
2018-07-14 17:25:00 +0800

    解决冲突

--- src/main/java/com/zkjl/wf_clserver/core/service/LogService.java
+++ src/main/java/com/zkjl/wf_clserver/core/service/LogService.java
@@ -7,11 +7,10 @@ import org.springframework.data.domain.PageImpl;
 import java.util.Date;
 
 public interface LogService {
- public void createLog(Log log);
  /**
   * 查找日志
   */
  PageImpl<Log> findPage(Integer pageSize, Integer pageNum, String username, Date beginDate, Date endDate);
 
- public void deleteLog(String id);
+ void deleteLog(String id);
 }
